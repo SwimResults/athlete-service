bcae819ec810bfba23000c4359dbd98070184d7c
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-04-17 07:43:18 +0300

    expand athlete model (dsv-id; firstname, lastname)

--- model/athlete.go
+++ model/athlete.go
@@ -5,6 +5,9 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Athlete struct {
 	Identifier primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name       string             `json:"name,omitempty" bson:"name,omitempty"`
+	Firstname  string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname   string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
 	Year       int                `json:"year,omitempty" bson:"year,omitempty"`
-	Team       Team               `json:"team,omitempty" bson:"team,omitempty"` // json only
+	DsvId      int                `json:"dsv_id,omitempty" bson:"dsv_id,omitempty"`
+	Team       Team               `json:"team,omitempty" bson:"team,omitempty"`
 }
