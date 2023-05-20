22af2e6c2a08ee76f6c1e54c5a870a9d64a9d19c
konrad2002 <konrad@schwimmteamerzgebirge.de>
2023-05-20 17:53:22 +0200

    fix email json name

--- model/contact.go
+++ model/contact.go
@@ -2,7 +2,7 @@ package model
 
 type Contact struct {
 	Name  string `json:"name,omitempty" bson:"name,omitempty"`
-	EMail string `json:"e_mail,omitempty" bson:"e_mail,omitempty"`
+	EMail string `json:"email,omitempty" bson:"email,omitempty"`
 	Phone string `json:"phone,omitempty" bson:"phone,omitempty"`
 	Fax   string `json:"fax,omitempty" bson:"fax,omitempty"`
 }
