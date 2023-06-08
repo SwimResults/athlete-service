f79e400fb440b194bd02fa8da04ae1de9b25c329
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-06-09 00:32:44 +0200

    athlete client

---
+++ client/athlete_client.go
@@ -0,0 +1,42 @@
+package client
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/swimresults/athlete-service/dto"
+	"github.com/swimresults/athlete-service/model"
+	"github.com/swimresults/service-core/client"
+	"net/http"
+)
+
+type AthleteClient struct {
+	apiUrl string
+}
+
+func NewAthleteClient(url string) *AthleteClient {
+	return &AthleteClient{apiUrl: url}
+}
+
+func (c *AthleteClient) importAthlete(athlete model.Athlete, meeting string) (*model.Athlete, error) {
+	request := dto.ImportAthleteRequestDto{
+		Meeting: meeting,
+		Athlete: athlete,
+	}
+
+	res, err := client.Post(c.apiUrl, "athlete/import", request)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	newAthlete := &model.Athlete{}
+	err = json.NewDecoder(res.Body).Decode(newAthlete)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusCreated && res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("import request returned: %d", res.StatusCode)
+	}
+	return newAthlete, nil
+}
