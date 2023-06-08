0829e66117875d097e90f42741bbad9dce673ed5
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-06-08 20:15:52 +0200

    athlete model add alias array

--- model/athlete.go
+++ model/athlete.go
@@ -1,12 +1,15 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Athlete struct {
 	Identifier    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`                     // automatically
 	Name          string             `json:"name,omitempty" bson:"name,omitempty"`                   // DSV-File + PDF
 	Firstname     string             `json:"firstname,omitempty" bson:"firstname,omitempty"`         // DSV-File + (PDF) 	!needs update!
 	Lastname      string             `json:"lastname,omitempty" bson:"lastname,omitempty"`           // DSV-File + (PDF) 	!needs update!
+	Alias         []string           `json:"alias,omitempty" bson:"alias,omitempty"`                 // semiautomatically
 	Year          int                `json:"year,omitempty" bson:"year,omitempty"`                   // DSV-File + PDF
 	Gender        string             `json:"gender,omitempty" bson:"gender,omitempty"`               // DSV-File + (PDF) 	!needs update!
 	DsvId         int                `json:"dsv_id,omitempty" bson:"dsv_id,omitempty"`               // DSV-File 			!needs update!
--- model/athlete_aliases.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type AthleteAliases struct {
-	Firstname string `json:"firstname,omitempty"`
-	Lastname  string `json:"lastname,omitempty"`
-	FullName  string `json:"full_name,omitempty"`
-}
