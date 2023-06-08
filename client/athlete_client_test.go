2271b6d7e2e89a8af11b3084e891fc8b72b87a76
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-06-09 00:34:43 +0200

    remove int tests

---
+++ client/athlete_client_test.go
@@ -0,0 +1,16 @@
+package client
+
+//func TestImportAthlete(t *testing.T) {
+//	athlete := model.Athlete{
+//		Name:  "   Simon Meier",
+//		Team:  model.Team{Name: "blub Team"},
+//		Year:  1234,
+//		DsvId: 123444,
+//	}
+//	c := NewAthleteClient("http://localhost:8086/")
+//	newAthlete, err := c.importAthlete(athlete, "IESC13")
+//	if err != nil {
+//		fmt.Printf(err.Error())
+//	}
+//	fmt.Printf("id: %s, name: %s, part: %s", newAthlete.Identifier.String(), newAthlete.Name, newAthlete.Participation)
+//}
--- client/team_client_test.go
+++ client/team_client_test.go
@@ -1,20 +1,16 @@
 package client
 
-import (
-	"fmt"
-	"github.com/swimresults/athlete-service/model"
-	"testing"
-)
-
-func TestImportTeam(t *testing.T) {
-	team := model.Team{
-		Name:    "Blubteam",
-		Country: "GER",
-	}
-	c := NewTeamClient("http://localhost:8086/")
-	newTeam, err := c.importTeam(team, "IESC12")
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	fmt.Printf("id: %s, name: %s, part: %s", newTeam.Identifier.String(), newTeam.Name, newTeam.Participation)
-}
+//func TestImportTeam(t *testing.T) {
+//	team := model.Team{
+//		Name:    "  Blub Team ",
+//		Country: "GER",
+//		DsvId:   1234,
+//		StateId: 4,
+//	}
+//	c := NewTeamClient("http://localhost:8086/")
+//	newTeam, err := c.importTeam(team, "IESC12")
+//	if err != nil {
+//		fmt.Printf(err.Error())
+//	}
+//	fmt.Printf("id: %s, name: %s, part: %s", newTeam.Identifier.String(), newTeam.Name, newTeam.Participation)
+//}
