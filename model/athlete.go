37b36a1eecaa7a2f413a5d0db6dbd838ef90345b
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2023-06-08 18:37:08 +0200

    FirstEvent -> FirstMeeting

--- model/athlete.go
+++ model/athlete.go
@@ -3,15 +3,15 @@ package model
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Athlete struct {
-	Identifier    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name          string             `json:"name,omitempty" bson:"name,omitempty"`
-	Firstname     string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
-	Lastname      string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
-	Year          int                `json:"year,omitempty" bson:"year,omitempty"`
-	Gender        string             `json:"gender,omitempty" bson:"gender,omitempty"`
-	DsvId         int                `json:"dsv_id,omitempty" bson:"dsv_id,omitempty"`
-	TeamId        primitive.ObjectID `json:"-" bson:"team_id,omitempty"`
-	Team          Team               `json:"team,omitempty" bson:"-"`
-	FirstEvent    string             `json:"first_event,omitempty" bson:"first_event,omitempty"`
-	Participation []string           `json:"participation,omitempty" bson:"participation,omitempty"`
+	Identifier    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`                     // automatically
+	Name          string             `json:"name,omitempty" bson:"name,omitempty"`                   // DSV-File + PDF
+	Firstname     string             `json:"firstname,omitempty" bson:"firstname,omitempty"`         // DSV-File + (PDF) 	!needs update!
+	Lastname      string             `json:"lastname,omitempty" bson:"lastname,omitempty"`           // DSV-File + (PDF) 	!needs update!
+	Year          int                `json:"year,omitempty" bson:"year,omitempty"`                   // DSV-File + PDF
+	Gender        string             `json:"gender,omitempty" bson:"gender,omitempty"`               // DSV-File + (PDF) 	!needs update!
+	DsvId         int                `json:"dsv_id,omitempty" bson:"dsv_id,omitempty"`               // DSV-File 			!needs update!
+	TeamId        primitive.ObjectID `json:"-" bson:"team_id,omitempty"`                             // automatically
+	Team          Team               `json:"team,omitempty" bson:"-"`                                // DSV-File + PDF
+	FirstMeeting  string             `json:"first_meeting,omitempty" bson:"first_meeting,omitempty"` // automatically
+	Participation []string           `json:"participation,omitempty" bson:"participation,omitempty"` // automatically
 }
